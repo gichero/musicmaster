d6e900d13ff657d8be46028807305e4727d28d10
Amos <amosgathungu@gmail.com>
2017-09-01 21:57:59 -0400

    problems with spotify api key

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,14 @@ export class App extends Component {
     }
     search() {
         console.log('this.state', this.state);
+        //const TOKEN =
         const BASE_URL = 'https://api.spotify.com/v1/search?';
         const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
         console.log('FETCH_URL', FETCH_URL);
+        fetch(FETCH_URL, {
+            method: 'GET'
+        })
+        .then(response => console.log('response', response));
     }
 
     render(){
